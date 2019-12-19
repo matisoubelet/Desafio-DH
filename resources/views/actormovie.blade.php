c899f5b644f88ff7755ac815a5f9ac113657dbc5
Matias <matiassoubelet@gmail.com>
2019-12-19 10:48:44 -0300

    actores

--- resources/views/actormovie.blade.php
+++ resources/views/actormovie.blade.php
@@ -1,34 +1,36 @@
 @extends("plantilla")
 
 @section("principal")
+<div class="flex">
+  <div class="contenedor">
+    <div class="agregarPelicula">
+      @if (count($errors) > 0)
+      <div class="error">
+          <ul>
+              @foreach ($errors->all() as $error)
+                  <li>{{ $error }}</li>
+              @endforeach
+          </ul>
+      </div>
+  @endif
 
-  <div class="agregarPelicula">
+  <form class="" action="/actores" method="post" enctype="multipart/form-data">
+    {{csrf_field()}}
+    <label for="actor_id"><h4 class="form">Seleccione la pelicula donde actuo {{$actor->first_name}}:</h4></label>
+    <input name="actor_id" type="hidden" value="{{$actor->id}}">
+    <select class="" name="movie_id">
+      @foreach ($peliculas as $pelicula)
+      <option value="{{$pelicula->id}}">
+        {{$pelicula->title}}
+      </option>
+      @endforeach
 
-    @if (count($errors) > 0)
-    <div class="error">
-        <ul>
-            @foreach ($errors->all() as $error)
-                <li>{{ $error }}</li>
-            @endforeach
-        </ul>
-    </div>
-@endif
-
-<form class="" action="/actores" method="post" enctype="multipart/form-data">
-  {{csrf_field()}}
-  <label for="actor_id"><h4 class="form">Seleccione la pelicula donde actuo {{$actor->first_name}}:</h4></label>
-  <input name="actor_id" type="hidden" value="{{$actor->id}}">
-  <select class="" name="movie_id">
-    @foreach ($peliculas as $pelicula)
-    <option value="{{$pelicula->id}}">
-      {{$pelicula->title}}
-    </option>
-    @endforeach
-
-    <div class="">
-      <input type="submit" name="" value="Agregar a pelicula">
-    </div>
-</form>
-
+      <div class="">
+        <input type="submit" name="" value="Agregar a pelicula">
+      </div>
+  </form>
+  </div>
+  </div>
+</div>
 
 @endsection
--- resources/views/agregarActor.blade.php
+++ resources/views/agregarActor.blade.php
@@ -1,37 +1,39 @@
 @extends("plantilla")
 
 @section("principal")
+<div class="flex">
+  <div class="contenedor">
+    <div class="agregarPelicula">
 
-  <div class="agregarPelicula">
+      @if (count($errors) > 0)
+      <div class="error">
+          <ul>
+              @foreach ($errors->all() as $error)
+                  <li>{{ $error }}</li>
+              @endforeach
+          </ul>
+      </div>
+  @endif
+    <form class="" action="/agregarActor" method="post" enctype="multipart/form-data">
+      {{csrf_field()}}
+      <div class="text">
+        <label for="first_name"><h4 class="form">Nombre:</h4></label>
+        <input type="text" name="first_name" value="{{old("first_name")}}">
+      </div>
 
-    @if (count($errors) > 0)
-    <div class="error">
-        <ul>
-            @foreach ($errors->all() as $error)
-                <li>{{ $error }}</li>
-            @endforeach
-        </ul>
-    </div>
-@endif
+      <div class="text">
+        <label for="last_name"><h4 class="form">Apellido:</h4></label>
+        <input type="text" name="last_name" value="{{old("last_name")}}">
+      </div>
 
-<form class="" action="/agregarActor" method="post" enctype="multipart/form-data">
-  {{csrf_field()}}
-  <div class="text">
-    <label for="first_name"><h4 class="form">Nombre:</h4></label>
-    <input type="text" name="first_name" value="{{old("first_name")}}">
+
+      <div class="">
+        <input type="submit" name="" value="Agregar Pelicula">
+      </div>
+    </form>
   </div>
+</div>
 
-  <div class="text">
-    <label for="last_name"><h4 class="form">Apellido:</h4></label>
-    <input type="text" name="last_name" value="{{old("last_name")}}">
-  </div>
-
-
-  <div class="">
-    <input type="submit" name="" value="Agregar Pelicula">
-  </div>
-
-</form>
 
 
 @endsection
